66dff9294d9ae813ffe86f80831cb7f491f171cf
HexinHexin <32285422+Lhx94As@users.noreply.github.com>
2021-12-07 16:12:41 +0800

    Update data_preprocessing.py

--- model/data_preprocessing.py
+++ model/data_preprocessing.py
@@ -1,5 +1,4 @@
 import os
-import glob
 import torch
 import librosa
 import argparse
@@ -7,14 +6,13 @@ import subprocess
 import numpy as np
 from tqdm import tqdm
 from pydub import AudioSegment
-import soundfile as sf
 import s3prl.upstream.wav2vec2.hubconf as hubconf
 from sklearn.preprocessing import LabelEncoder
 
 
 def make_200ms_feat(mfccs, overlap=10, chunk_len=20):
     new_feat = 0
-    feature = mfccs.T
+    feature = mfccs
     seq_len = feature.shape[0]
     step = chunk_len - overlap
     num_chunk = (seq_len - overlap) // (chunk_len - overlap)
@@ -39,10 +37,9 @@ def make_200ms_feat(mfccs, overlap=10, chunk_len=20):
 
 def upsampling_lre(audio, save_dir):
     if audio.endswith('.sph'):
-        data, sr = librosa.load(audio, sr=None)
         new_name = save_dir + '/' + os.path.split(audio)[-1].replace('.sph', '.wav')
-        sf.write(new_name, data, 8000, subtype='PCM_16')
-        subprocess.call(f"sox {audio} -r 16000 {new_name}", shell=True)
+        subprocess.call(f"sph2pipe {audio} {new_name}", shell=True)
+        subprocess.call(f"sox {new_name} -r 16000 {new_name}", shell=True)
     elif audio.endswith('.wav') or audio.endswith('.WAV'):
         new_name = save_dir + '/' + os.path.split(audio)[-1].replace('.WAV', '.wav')
         subprocess.call(f"sox {audio} -r 16000 {new_name}", shell=True)
@@ -52,12 +49,14 @@ def main():
     parser.add_argument('--step', type=int, help='step to control the process. 0 means starting from beginning')
     parser.add_argument('--lredir', type=str, help='e.g.: lre_train/')
     parser.add_argument('--model', type=str, help='pretrained XLSR-53 model dir')
+    parser.add_argument('--kaldi', type=str, help='e.g.: /home/user_kk/kaldi/')
     parser.add_argument('--device',type = int, help='cuda id, default 0', default=0)
     parser.add_argument('--layer', type=int, help='Extract wav2vec feats from this layer', default=16)
     parser.add_argument('--seglen', type=int, help='segmentlength', default=30)
     parser.add_argument('--overlap', type=int, help='overlap length', default=1)
     parser.add_argument('--savedir', type=str, help='dir to save wav2vec feats', default=None)
     args = parser.parse_args()
+    os.environ["PATH"] += os.pathsep + "{}/tools/sph2pipe_v2.5/".format(args.kaldi)
 
     device = torch.device('cuda:{}'.format(args.device) if torch.cuda.is_available() else 'cpu')
     model = hubconf.wav2vec2_local(ckpt=args.model)
@@ -73,21 +72,22 @@ def main():
     if args.step <= 0:
         audio_list = []
         labels = []
-        if args.lredir.strip('/').endswith('Training_Data'):
+        lredir = args.lredir
+        if lredir.strip('/').endswith('Training_Data'):
             key_file = args.lredir + '/docs/filename_language_key.tab'
             with open(key_file, 'r') as f:
                 lines = f.readlines()
             labels_text = [x.split()[1].strip().replace('-', '') for x in lines]
             audio_list = [args.lredir + '/data/{}/{}'.format(x.split()[1].strip(),x.split()[0]) for x in lines]
             labels = le.transform(labels_text)
-        elif args.lredir.strip('/').endswith('Development_Data'):
+        elif lredir.strip('/').endswith('Development_Data'):
             key_file = args.lredir + '/docs/lre17_dev_segments.key'
             with open(key_file, 'r') as f:
                 lines = f.readlines()
             labels_text = [x.split()[1].strip().replace('-', '') for x in lines[1:]]
             audio_list = [args.lredir + '/data/dev/{}'.format(x.split()[0]) for x in lines[1:]]
             labels = le.transform(labels_text)
-        elif args.lredir.strip('/').endswith('Eval_Data'):
+        elif lredir.strip('/').endswith('Eval_Data'):
             key_file = args.lredir + '/docs/lre17_eval_segments.key'
             with open(key_file, 'r') as f:
                 lines = f.readlines()
@@ -166,9 +166,11 @@ def main():
                 try: # To skip some too long or too short utterances
                     features = model(data_)
                     features = features['hidden_state_{}'.format(args.layer)]
-                    save_name = audio.replace(args.audiodir, args.savedir).replace('.wav', '.npy')
-                    np.save(save_name, features.squeeze(0).cpu().detach().numpy())
-                    f.write("{} {} {}\n".format())
+                    features_ = features.squeeze(0).cpu().detach().numpy()
+                    save_name = audio.replace('.wav', '.npy')
+                    new_feat = make_200ms_feat(features_, overlap=0, chunk_len=20)
+                    np.save(save_name, new_feat)
+                    f.write("{} {} {}\n".format(save_name, label, features.shape[0]))
                 except:
                     print("Len:{} {} is not successful, skip this one".format(len(data) / sr, audio))
 
