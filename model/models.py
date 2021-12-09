bdda56d67ff502fd4c609809db6f3add629cec17
HexinHexin <32285422+Lhx94As@users.noreply.github.com>
2021-12-09 23:32:19 +0800

    Update models.py

--- model/models.py
+++ model/models.py
@@ -42,7 +42,7 @@ class X_Transformer_E2E_LID(nn.Module):
         self.fc2 = nn.Linear(self.d_model, self.d_model)
         self.fc3 = nn.Linear(self.d_model, n_lang)
 
-    def mean_std_pooling(self, x, batchsize, seq_lens, weight, mask_std, weight_unb):
+    def mean_std_pooling(self, x, batchsize, seq_lens, mask_mean, weight_mean, mask_std, weight_unb):
         """
 
         :param x: expect the x is of shape [Batchsize, seq_len, feature_dim]
@@ -56,15 +56,18 @@ class X_Transformer_E2E_LID(nn.Module):
         """
         max_len = seq_lens[0]
         feat_dim = x.size(-1)
-        correct_mean = x.mean(dim=1).transpose(0, 1) * weight
+        if mask_mean is not None:
+            assert mask_mean.size() == x.size()
+            x.masked_fill_(mask_mean, 0)
+        correct_mean = x.mean(dim=1).transpose(0, 1) * weight_mean
         correct_mean = correct_mean.transpose(0, 1)
         center_seq = x - correct_mean.repeat(1, 1, max_len).view(batchsize, -1, feat_dim)
-        variance = torch.mean(torch.mul(torch.abs(center_seq) ** 2, mask_std), dim=1).transpose(0,
-                                                                                                1) * weight_unb * weight
+        variance = torch.mean(torch.mul(torch.abs(center_seq) ** 2, mask_std), dim=1).transpose(0,1) \
+                   * weight_unb * weight_mean
         std = torch.sqrt(variance.transpose(0, 1))
         return torch.cat((correct_mean, std), dim=1)
 
-    def forward(self, x, seq_len, seq_weights, std_mask_, weight_unbaised, atten_mask=None, eps=1e-5):
+    def forward(self, x, seq_len, mean_mask_, weight_mean, std_mask_, weight_unbaised, atten_mask=None, eps=1e-5):
         batch_size = x.size(0)
         T_len = x.size(1)
         x = self.dropout(x)
@@ -81,7 +84,6 @@ class X_Transformer_E2E_LID(nn.Module):
             x += noise * eps
 
         stats = torch.cat((x.mean(dim=2), x.std(dim=2)), dim=1)
-        # print("pooling", stats.size())
         embedding = self.fc_xv(stats)
         embedding = embedding.view(batch_size, T_len, self.feat_dim)
         output = self.layernorm1(embedding)
@@ -91,9 +93,7 @@ class X_Transformer_E2E_LID(nn.Module):
         output = output.transpose(1, 2).contiguous().view(batch_size, -1, self.d_model)
         output, _ = self.attention_block1(output, atten_mask)
         output, _ = self.attention_block2(output, atten_mask)
-        # output, _ = self.attention_block3(output, atten_mask)
-        # output, _ = self.attention_block4(output, atten_mask)
-        stats = self.mean_std_pooling(output, batch_size, seq_len, seq_weights, std_mask_, weight_unbaised)
+        stats = self.mean_std_pooling(output, batch_size, seq_len, mean_mask_, weight_mean, std_mask_, weight_unbaised)
         output = F.relu(self.fc1(stats))
         output = F.relu(self.fc2(output))
         output = self.fc3(output)
